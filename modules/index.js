fb5df81bbec7d94840218e74192ba0851d4eb0f6
a3magic3pocket <a3magic3pocket3@gmail.com>
2021-11-01 09:59:00 +0900

    Set default next settings.

---
+++ modules/index.js
@@ -0,0 +1,16 @@
+import { all, fork } from "redux-saga/effects";
+import { combineReducers } from "redux";
+// import requests, { requestSagas } from "./common/request";
+// import loading from "./common/loading";
+
+const rootReducer = combineReducers({
+  // loading,
+  // ...requests,
+});
+
+export function* rootSaga() {
+  // yield all([...requestSagas.map((f) => fork(f))]);
+  yield all([]);
+}
+
+export default rootReducer;
---
+++ store/configureStore.js
@@ -0,0 +1,23 @@
+import { createWrapper } from "next-redux-wrapper";
+import { applyMiddleware, compose, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer, { rootSaga } from "../modules";
+import createSagaMiddleware from "redux-saga";
+
+const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(rootReducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+  return store;
+};
+
+const wrapper = createWrapper(configureStore, {
+  debug: process.env.NODE_ENV === "development",
+});
+
+export default wrapper;
