c863f6509f2e5453d9258cfbb36a2f5fb5ec6e47
a3magic3pocket <a3magic3pocket@gmail.com>
2021-11-02 23:11:22 +0900

    Set coordinates of popup window.

--- components/common/login.js
+++ components/common/login.js
@@ -2,11 +2,17 @@ import { useLogout } from "../../hookes/auth";
 import { DefaultButton } from "./button";
 
 export function LoginButton({ className, children }) {
+  // Reference by : https://stackoverflow.com/questions/10380015/set-window-popup-coordinates
   const handleLogin = () => {
+    const width = 320;
+    const height = 400;
+    const top = parseInt(screen.availHeight / 2 - height / 2);
+    const left = parseInt(screen.availWidth / 2 - width / 2);
+
     window.open(
       "//localhost:8080/login?redirect-url=localhost:3000/check",
       "SIMPLE-LOCKER login",
-      "scrollbars=no,width=320,height=400,menubar=false,resizable=yes"
+      `scrollbars=no,width=${width},height=${height},menubar=false,resizable=yes,top=${top},left=${left}`
     );
   };
   return (
